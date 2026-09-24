1ce324713ca8637a5443725b7176c98f35050874
Huanhuan Huang <huahuang@g.hmc.edu>
2026-09-24 16:19:49 -0700

    added V parameter to specialcase

--- src/fpu/postproc/postprocess.sv
+++ src/fpu/postproc/postprocess.sv
@@ -217,7 +217,7 @@ module postprocess import cvw::*;  #(parameter cvw_t P, V) (
 
   negateintres #(P) negateintres(.Xs, .Shifted, .Signed, .Int64, .Plus1, .CvtNegResMsbs, .CvtNegRes);
 
-  specialcase #(P) specialcase(.Xs, .Xm, .Ym, .Zm, .XZero, .IntInvalid,
+  specialcase #(P, V) specialcase(.Xs, .Xm, .Ym, .Zm, .XZero, .IntInvalid,
       .IntZero, .Frm, .OutFmt, .XNaN, .YNaN, .ZNaN, .CvtResUf,
       .NaNIn, .IntToFp, .Int64, .Signed, .Zfa, .CvtOp, .FmaOp, .Plus1, .Invalid, .Overflow, .InfIn, .CvtNegRes,
       .XInf, .YInf, .DivOp, .DivByZero, .FullRe, .CvtCe, .Rs, .Re, .Rf, .PostProcRes, .FCvtIntRes);
